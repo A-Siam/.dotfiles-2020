2e3d4b2e0b21ab45160ca20dae2518aa5ae77bac
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-10-20 04:32:50 +0200

    fish config

---
+++ fish/config.fish
@@ -0,0 +1,36 @@
+set fish_greeting
+
+set -x CM_DEBUG 1
+set -x CM_MAX_CLIPS 100
+set -x CM_HISTLENGTH 20
+set -x CM_SELECTIONS "clipboard primary"
+
+set -x BROWSER firefox
+# set -x TERMINAL alacritty
+set -x TERMINAL urxvt
+set -x TERM xterm-256color
+set -x FILEEXP ranger
+
+# aliases
+alias jn="condaInit && jupyter notebook"
+alias si="sudo pacman -S"
+alias sup="sudo pacman -Syu"
+alias gfp="cd /run/media/$USER/MyProjects/flask_setup"
+alias ss="pacman -Ss"
+alias t="$TERMINAL -e sh -c 'zsh' &"
+alias gmm="cd /run/media/$USER/MyFiles/Music"
+alias yi="yay -S"
+alias gmp="cd /run/media/$USER/MyProjects"
+alias srm="sudo pacman -Runs"
+alias grb="cd /home/asiam/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book"
+alias gmf="cd /run/media/asiam/MyFiles"
+alias gmc="cd /run/media/asiam"
+
+# oracle xe env vars
+set -x ORACLE_HOME /opt/oracle/product/18c/dbhomeXE
+set -x ORACLE_SID XE
+set -x PATH $PATH:$ORACLE_HOME/bin
+
+# tasks 
+echo "don't forget to migrate with dev-db-connection whenever it changes"
+

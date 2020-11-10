e4092ce0cd7346f120c7041f8adda16c48434018
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-11-10 21:21:24 +0200

    fix: fish se

--- fish/config.fish
+++ fish/config.fish
@@ -4,22 +4,28 @@ set -x CM_DEBUG 1
 set -x CM_MAX_CLIPS 100
 set -x CM_HISTLENGTH 20
 set -x CM_SELECTIONS "clipboard primary"
+set -x WINEPREFIX ~/win32
 
 set -x BROWSER firefox
-# set -x TERMINAL alacritty
 set -x TERMINAL urxvt
 set -x TERM xterm-256color
 set -x FILEEXP ranger
 set -x EDITOR vim
 
+set -x SQLPLUS_BIN (which sqlplus)
 # aliases
+alias gof="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/"
+alias word="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/ && wine WINWORD.EXE"
+alias powerpoint="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/ && wine POWERPNT.EXE"
 alias jn="condaInit && jupyter notebook"
 alias si="sudo pacman -S"
 alias sup="sudo pacman -Syu"
 alias gfp="cd /run/media/$USER/MyProjects/flask_setup"
 alias ss="pacman -Ss"
-alias t="$TERMINAL -e sh -c 'zsh' &"
+alias t="$TERMINAL -e sh -c 'fish' &"
 alias gmm="cd /run/media/$USER/MyFiles/Music"
+alias gma="cd /run/media/$USER/MyFiles/CSE_Acad"
+alias gms="cd /run/media/$USER/MyFiles/CSE_selfstudy"
 alias yi="yay -S"
 alias gmp="cd /run/media/$USER/MyProjects"
 alias srm="sudo pacman -Runs"
@@ -33,11 +39,12 @@ alias ss="sudo su"
 alias ll="ls -l"
 alias jn="conda_init && jupyter notebook"
 alias cr="cd /run/media/asiam/MyFiles/conf/"
+alias sf="~/.scripts/sf"
 
 # oracle xe env vars
 set -x ORACLE_HOME /opt/oracle/product/18c/dbhomeXE
 set -x ORACLE_SID XE
-set -x PATH $PATH:$ORACLE_HOME/bin
+set -x PATH $PATH:$ORACLE_HOME/bin:$HOME/bin
 
 function conda_init;
 # >>> conda initialize >>>
@@ -50,4 +57,3 @@ end
 set PATH /home/asiam/.local/bin $PATH
 fish_vi_key_bindings
 
-echo "we can use the video summarization from the prev year's project"

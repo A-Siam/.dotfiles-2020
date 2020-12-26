eaada61460d8c141ff00916291be322adbe4cdd6
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-12-26 22:05:59 +0200

    vscode open here alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,16 +4,18 @@ set -x CM_DEBUG 1
 set -x CM_MAX_CLIPS 100
 set -x CM_HISTLENGTH 20
 set -x CM_SELECTIONS "clipboard primary"
+set -x WINEARCH win32
 set -x WINEPREFIX ~/win32
 
 set -x BROWSER firefox
-set -x TERMINAL alacritty
+set -x TERMINAL urxvt
 set -x TERM xterm-256color
 set -x FILEEXP ranger
 set -x EDITOR vim
 set -x npm_config_prefix $HOME/.node_modules
 
 # aliases
+alias ch="code . -r"
 alias gof="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/"
 alias word="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/ && wine WINWORD.EXE"
 alias powerpoint="cd ~/win32/drive_c/Program\ Files/Microsoft\ Office/Office12/ && wine POWERPNT.EXE"
@@ -29,12 +31,12 @@ alias gms="cd /run/media/$USER/MyFiles/CSE_selfstudy"
 alias yi="yay -S"
 alias gmp="cd /run/media/$USER/MyProjects"
 alias srm="sudo pacman -Runs"
-alias grb="cd /home/asiam/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book"
-alias gmf="cd /run/media/asiam/MyFiles"
-alias gmc="cd /run/media/asiam"
+alias grb="cd /home/a/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/book"
+alias gmf="cd /run/media/a/MyFiles"
+alias gmc="cd /run/media/a"
 alias p="python"
-alias gss="cd /run/media/asiam/MyProjects/Server-side"
-alias gmfsnd="cd /run/media/asiam/MyProjects/_MyFSND"
+alias gss="cd /run/media/a/MyProjects/Server-side"
+alias gmfsnd="cd /run/media/a/MyProjects/_MyFSND"
 alias ss="sudo su"
 alias ll="ls -l"
 alias jn="conda_init && jupyter notebook"
@@ -65,3 +67,7 @@ end
 # add normal python binaries to our path
 set PATH /home/asiam/.local/bin $PATH
 fish_vi_key_bindings
+
+# notes
+echo "NOTES:"
+echo "we are going to have 2 quizes in parallel"

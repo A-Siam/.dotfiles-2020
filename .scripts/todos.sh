9c014f0e67db620bcfc5a602288dd1d804ad65f1
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-05-27 15:06:57 +0200

    update scripts

---
+++ .scripts/todos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "TODOs"
+echo "- [x] create models"
+echo "- [ ] create end-point similar to the one in client side"
+echo "- [ ] call ml server similar in the appropriete places"
